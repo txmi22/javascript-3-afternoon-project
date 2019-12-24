08891d76df32ff10f7ed679ea94d1a591ae09599
Tommy Manyvanh <tommy_manyvanh@yahoo.com>
2019-12-23 16:40:26 -0800

    finished higher order js

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+let evenNumbers = mixedNumbers.filter(v=>v%2===0);
+
+
+
 
 
 
@@ -44,7 +47,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(v=>v*1.07);
+    
+    
 
 
 
@@ -63,7 +68,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((a, b) =>a+b);
+ 
+    //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -89,7 +96,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(v=>v.CP>200); // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
@@ -106,7 +113,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(v=>v.price+(v.price*v.tax));
 
 
 
@@ -126,6 +133,9 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(v=>v['owner']==='Bob').reduce((x, y)=>{
+	x=+x+ +y.price
+	return x; 
+}, []); 
 
 
